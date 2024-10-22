dd4bab7ec0ed49379ce4c95464fc86acc5086920
hoah2333 <z13725385251@163.com>
2024-10-22 11:37:20 +0800

    1.0.0：发布自定义回复插件

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from "koishi";
 
-export const name = 'custom-reply'
+export const name = "custom-reply";
 
-export interface Config {}
+export interface Config {
+    template: { command: string; description: string; reply: string }[];
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+    template: Schema.array(
+        Schema.object({
+            command: Schema.string().required().description("触发指令"),
+            description: Schema.string().description("指令描述"),
+            reply: Schema.string()
+                .role("textarea", { rows: [4, 10] })
+                .required()
+                .description("对应回复")
+        })
+    ).description("自定义指令回复列表")
+}).description("自定义指令回复");
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export function apply(ctx: Context, config: Config): void {
+    for (const template of config.template) {
+        ctx.command(template.command, template.description || "").action((): string => {
+            return template.reply;
+        });
+    }
 }
